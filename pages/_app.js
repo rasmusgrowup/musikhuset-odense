ad271aa1aef94656fd4b1a18ea5d5608c5068d42
Rasmus Andersen <rasmus@growupstudio.dk>
2024-01-17 16:06:49 +0100

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import '@/styles/globals.css'
 import Layout from "@/components/Layout";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
       <Layout>
           <Navbar/>
           <Component {...pageProps} />
+          <Analytics />
           <Footer />
       </Layout>
   )
